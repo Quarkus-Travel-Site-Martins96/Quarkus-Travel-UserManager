0266d176b3a8a2b84c1acb137e1a8679ee3ef7b1
Martins96 <luca.martinelli7cc@gmail.com>
2021-03-03 22:06:51 +0100

    First commit

---
+++ src/test/java/com/lucamartinelli/app/travelsite/NativeusermanagerUserManagerIT.java
@@ -0,0 +1,9 @@
+package com.lucamartinelli.app.travelsite;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeusermanagerUserManagerIT extends usermanagerUserManagerTest {
+
+    // Execute the same tests but in native mode.
+}
